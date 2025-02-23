6504f57a166a03d030ee5ad73f3ffe93f52132f3
chromecodes <hameedhussain782@gmail.com>
2025-02-23 23:47:36 +0530

    working on new apis

---
+++ src/app/api/settings/profile/route.ts
@@ -0,0 +1,98 @@
+import { connectDB } from "@/dbConfig/dbConfig";
+import { NextResponse, NextRequest } from "next/server";
+import Form from "@/models/formModel";
+import User from "@/models/userModel";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { use } from "react";
+import exp from "constants";
+
+export async function GET(request: NextRequest) {
+  try {
+    // // Authentication check start
+    // Get session and cookies
+    const cookies = request.cookies as any;
+    const token = cookies.get("token");
+
+    await connectDB();
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const tokenData: any = jwt.verify(token?.value!, process.env.TOKEN_SECRET!);
+
+    // Find the user
+    const CurrentUser = await User.findOne({ _id: tokenData._id });
+
+    if (!CurrentUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(
+      { success: true, data: CurrentUser.notificationSettings },
+      {
+        status: 200,
+        headers: {
+          "Cache-Control": "no-store", // Prevent caching of sensitive data
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Form creation error:", error);
+    return NextResponse.json(
+      { success: false, error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    // // Authentication check start
+    // Get session and cookies
+    const cookies = request.cookies as any;
+    const token = cookies.get("token");
+
+    await connectDB();
+
+    if (!token) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    const tokenData: any = jwt.verify(token?.value!, process.env.TOKEN_SECRET!);
+
+    // Find the user
+    const CurrentUser = await User.findOne({ _id: tokenData._id });
+
+    if (!CurrentUser) {
+      return NextResponse.json({ message: "User not found" }, { status: 404 });
+    }
+
+    // Update the notification settings
+
+    const notificationSettings = request.body as any;
+
+    const updatedUser = await User.findByIdAndUpdate(
+      tokenData._id,
+      { notificationSettings },
+      { new: true }
+    );
+
+    return NextResponse.json(
+      { success: true, data: CurrentUser.notificationSettings },
+      {
+        status: 200,
+        headers: {
+          "Cache-Control": "no-store", // Prevent caching of sensitive data
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Form creation error:", error);
+    return NextResponse.json(
+      { success: false, error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
